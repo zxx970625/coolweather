85c5541ba81e2f3519efb28af7792ee5c54ce70e
zxx970625 <1394452815@qq.com>
2018-06-15 13:54:43 +0800

    Initial commit.

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,43 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceid;
+
+    public int getid() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceid() {
+        return provinceid;
+    }
+
+    public void setProvinceid(int provinceid) {
+        this.provinceid = provinceid;
+    }
+}
+
